b89eadfc04e3fcbece3c68245a74569c34ad916a
Mayank Sethi <mayank@digibrain.co.in>
2024-04-21 16:41:01 +0530

    add meta info

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <meta name="og:title" content="Little Lemon" />
     <App />
   </React.StrictMode>
 );
